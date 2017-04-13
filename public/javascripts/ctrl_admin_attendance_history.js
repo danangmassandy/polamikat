37b0a5348c5062edc8e968cafa7eb052ac550e24
Danang Massandy <danangmassandy@gmail.com>
2017-04-13 21:50:58 +0700

    Download as csv

--- public/javascripts/ctrl_admin_attendance_history.js
+++ public/javascripts/ctrl_admin_attendance_history.js
@@ -1,5 +1,56 @@
 var app = angular.module('polamikatApp');
 
+function convertArrayOfObjectsToCSV(args) {  
+    var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+    data = args.data || null;
+    if (data == null || !data.length) {
+        return null;
+    }
+
+    columnDelimiter = args.columnDelimiter || ',';
+    lineDelimiter = args.lineDelimiter || '\n';
+
+    keys = Object.keys(data[0]);
+
+    result = '';
+    result += keys.join(columnDelimiter);
+    result += lineDelimiter;
+
+    data.forEach(function(item) {
+        ctr = 0;
+        keys.forEach(function(key) {
+            if (ctr > 0) result += columnDelimiter;
+
+            result += item[key];
+            ctr++;
+        });
+        result += lineDelimiter;
+    });
+
+    return result;
+}
+
+function downloadCSV(args, csv) {  
+    var data, filename, link;
+    // var csv = convertArrayOfObjectsToCSV({
+    //     data: data
+    // });
+    if (csv == null) return;
+
+    filename = args.filename || 'export.csv';
+
+    if (!csv.match(/^data:text\/csv/i)) {
+        csv = 'data:text/csv;charset=utf-8,' + csv;
+    }
+    data = encodeURI(csv);
+
+    link = document.createElement('a');
+    link.setAttribute('href', data);
+    link.setAttribute('download', filename);
+    link.click();
+}
+
 app.controller('adminAttendanceHistoryCtrl', function ($scope, $rootScope, $mdDialog, $mdSidenav, $timeout, $location, showMessage, globalConstant, rest) {
     if(!$rootScope.me.isAdmin){
         showMessage.accessDenied(null,
@@ -51,6 +102,32 @@ app.controller('adminAttendanceHistoryCtrl', function ($scope, $rootScope, $mdDi
 
     $scope.onCurrDateChanged = function(newValue, oldValue) {
         $scope.loadAttendanceByPeriod();
+    }
+
+    $scope.downloadDataAsCSV = function() {
+        var data = [];
+        for (var i = 0; i < $scope.attendanceListData.length; ++i) {
+            data.push({
+                No : i + 1,
+                NRP : $scope.attendanceListData[i].nrp,
+                Pangkat : $scope.attendanceListData[i].pangkat,
+                Nama : $scope.attendanceListData[i].name,
+                Hadir : $scope.attendanceListData[i].totalPresent,
+                Ijin : $scope.attendanceListData[i].totalOnLeave,
+                Sakit : $scope.attendanceListData[i].totalMedicalLeave,
+                'Tanpa Keterangan' : $scope.attendanceListData[i].totalWithoutNotice,
+                Total : $scope.attendanceListData[i].total
+            });
+        }
+        var csv = convertArrayOfObjectsToCSV({
+            data: data
+        });
+
+        // generate filename
+        var filename;
+        downloadCSV({
+            filename : filename
+        }, csv);
     }
 
 });
